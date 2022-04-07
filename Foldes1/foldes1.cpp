cf085050ed154595a6bbb5d534ad36e2d2157bd4
Alboriy <alboriy@list.ru>
2022-04-08 00:55:42 +0500

    last commit

--- Foldes1/foldes1.cpp
+++ Foldes1/foldes1.cpp
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 #include <iostream>
 
 void SomeCode() {
 	int a = 4, b = 4;
 	std::cout << a + b << std::endl;
 	std::cout << a * b << std::endl;
+	std::cout << a / b << std::endl;
 	std::cout << a - b << std::endl;
 }
-=======
->>>>>>> parent of 3ae2678 (SomeCode functions that sum a b)
